3a24ff2239bd8909e3184d08397707fd10aedd63
Muhammad chandra zulfikar <Chanjull19@gmail.com>
2019-12-20 22:46:16 +0700

    change title

--- src/constant/app.config.ts
+++ src/constant/app.config.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   API_URL: 'https://pokeapi.co/api/v2/',
-  PUBLIC_PATH: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/female',
+  PUBLIC_PATH: '/',
   URL_PNG: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon'
 };
-export const PUBLIC_URL = process.env.NODE_ENV === 'production' ? process.env.PUBLIC_PATH : ''
+// export const PUBLIC_URL = process.env.NODE_ENV === 'production' ? process.env.PUBLIC_PATH : ''
